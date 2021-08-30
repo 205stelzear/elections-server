f25c8277096342ba8ca81084b01b4df1ae8c4085
V-ed <guillaumemarcoux@gmail.com>
2021-08-29 22:05:28 -0400

    Fix PrismaService not being imported by virtual election module

--- src/virtual-election/virtual-election.module.ts
+++ src/virtual-election/virtual-election.module.ts
@@ -1,10 +1,11 @@
 import { ElectionService } from '$/election/election.service';
+import { PrismaService } from '$/prisma.service';
 import { Module } from '@nestjs/common';
 import { VirtualElectionController } from './virtual-election.controller';
 import { VirtualElectionService } from './virtual-election.service';
 
 @Module({
-	providers: [VirtualElectionService, ElectionService],
+	providers: [VirtualElectionService, ElectionService, PrismaService],
 	controllers: [VirtualElectionController],
 })
 export class VirtualElectionModule {}
